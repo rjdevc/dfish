1b1c760ea0f54c320eb44a3c36a4e3537135d3c4
lamontYu <31650623@qq.com>
2018-10-17 16:03:53 +0800

    合并进度条数据序列号

--- dfish/dfish-framework/src/main/java/com/rongji/dfish/framework/plugin/progress/ProgressData.java
+++ dfish/dfish-framework/src/main/java/com/rongji/dfish/framework/plugin/progress/ProgressData.java
@@ -1,8 +1,13 @@
 package com.rongji.dfish.framework.plugin.progress;
 
+import java.io.Serializable;
+
 import com.rongji.dfish.ui.Command;
 
-public class ProgressData {
+public class ProgressData implements Serializable {
+	
+	private static final long serialVersionUID = -727048341094069751L;
+	
 	private String progressKey;
 	private double[] stepScales;
 	private int stepIndex;
