3bc3d5537b9d945c608c608098440e714d33c8e6
linXuan <fjfz_linxuan@sohu.com>
2018-12-29 16:02:59 +0800

    html增加format参数

--- dfish/dfish-ui/src/main/java/com/rongji/dfish/ui/widget/Html.java
+++ dfish/dfish-ui/src/main/java/com/rongji/dfish/ui/widget/Html.java
@@ -21,6 +21,8 @@ public class Html extends AbstractWidget<Html> implements Scrollable<Html>,HtmlC
 	private String scrollClass;
 	private Boolean escape;
 	private String thumbwidth;
+	private String format;
+	
 	/**
 	 * 构造函数
 	 * @param id  自定义的ID。可通过 view.find( id ) 方法来获取 widget。
@@ -139,4 +141,36 @@ public class Html extends AbstractWidget<Html> implements Scrollable<Html>,HtmlC
     	this.thumbwidth = String.valueOf(thumbwidth);
 	    return   this;
     }
+	
+	/**
+	 * 格式化内容。支持替换 "$field" 和 "${field.prop}" 形式的变量。支持"javascript:"开头的js语句(需return返回值)。
+	 * 如果列表有多行，并且这个字段显示的时候，需要一个复杂HTML，而每行中需要的变化的仅仅是少量的数据，可以使用format来减少传输量。
+	 * 典型的有两种写法
+	 * <pre>
+	 * javascript:var d= this.x.data.s;if('1'==d){return \"&lt;span style='color:gray'&gt;唯一&lt;/span&gt;\"};return '';
+	 * </pre>或<pre>
+	 * [&lt;a href='javascript:;' onclick=\"demo.enterView(this,'$vId');\"&gt;查看&lt;/a&gt;]&amp;nbsp;
+	 * </pre>
+	 * 
+	 * @return String
+ 	 */
+ 	public Object getFormat() {
+ 		return format;
+ 	}
+ 	/**
+ 	 * 格式化内容。支持替换 "$field" 和 "${field.prop}" 形式的变量。支持"javascript:"开头的js语句(需return返回值)。
+ 	 * 如果列表有多行，并且这个字段显示的时候，需要一个复杂HTML，而每行中需要的变化的仅仅是少量的数据，可以使用format来减少传输量。
+ 	 * 典型的有两种写法
+ 	 * <pre>
+ 	 * javascript:var d= this.x.data.s;if('1'==d){return \"&lt;span style='color:gray'&gt;唯一&lt;/span&gt;\"};return '';
+ 	 * </pre>或<pre>
+ 	 * [&lt;a href='javascript:;' onclick=\"demo.enterView(this,'$vId');\"&gt;查看&lt;/a&gt;]&amp;nbsp;
+ 	 * </pre>
+ 	 * @param format String
+ 	 * @return 本身，这样可以继续设置其他属性
+ 	 */
+ 	public Html setFormat(String format) {
+ 		this.format = format;
+ 		return this;
+ 	}
 }
