d2d7bf6d39c0e99d3a536eaafd8425f85d870653
LinLW <27542726@qq.com>
2019-01-14 11:52:36 +0800

    解决提交冲突

--- dfish/dfish-ui/src/main/java/com/rongji/dfish/ui/LazyLoad.java
+++ dfish/dfish-ui/src/main/java/com/rongji/dfish/ui/LazyLoad.java
@@ -6,31 +6,18 @@ package com.rongji.dfish.ui;
  *
  * @param <T> 当前对象类型
  */
-public interface LazyLoad<T extends LazyLoad<T>> {
+public interface LazyLoad<T extends LazyLoad<T>> extends HasSrc<T> {
 	/**
 	 * 加载 具体内容 的 url。访问这个url 时应当返回一个 json 字串。
 	 * 如果没有template 这个字符串应该是dfish的格式。
 	 * 如果有template 那么template 讲把这个字符串解析成dfish需要的格式。
 	 * @return String
 	 */
-	String getSrc();
-	/**
-	 * 加载 具体内容 的 url。访问这个url 时应当返回一个 json 字串。
-	 * 如果没有template 这个字符串应该是dfish的格式。
-	 * 如果有template 那么template 讲把这个字符串解析成dfish需要的格式。
-	 * @param src url
-	 * @return this
-	 */
-	T setSrc(String src);
-	/**
-	 * 指定用这个schema编号所对应的schema 将src返回的内容解析成dfish的格式。
-	 * @return String
-	 */
-	String getSchema();
+	String getTemplate();
 	/**
 	 * 指定用这个schema编号所对应的schema 将src返回的内容解析成dfish的格式。
 	 * @param schema String
 	 * @return this
 	 */
-	T setSchema(String schema);
+	T setTemplate(String template);
 }
--- dfish/dfish-ui/src/main/java/com/rongji/dfish/ui/widget/TemplateTitle.java
+++ dfish/dfish-ui/src/main/java/com/rongji/dfish/ui/widget/TemplateTitle.java
@@ -1,92 +1,22 @@
 package com.rongji.dfish.ui.widget;
 
-import com.rongji.dfish.ui.AbstractWidget;
-import com.rongji.dfish.ui.HasText;
-import com.rongji.dfish.ui.HtmlContentHolder;
-import com.rongji.dfish.ui.Scrollable;
-
 /**
  * TemplateTitle 用于弹出窗口模板中顶部标题栏目的部分
  * 
  * @author DFish Team
- *
+ * @deprecated 3.2改版后用DialogTitle代替
  */
-public class TemplateTitle extends AbstractWidget<TemplateTitle> implements Scrollable<TemplateTitle>,HtmlContentHolder<TemplateTitle>,HasText<TemplateTitle>{
+@Deprecated
+public class TemplateTitle extends DialogTitle{
+
+	public TemplateTitle(String text) {
+		super(text);
+	}
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -8155744934245686731L;
-	private Boolean escape;
-	private String text;
-	private Boolean scroll;
-	private String scrollClass;
-	
-	/**
-	 * 构造函数
-	 * @param id String 编号
-	 * @param text String 显示文本
-	 */
-	public TemplateTitle(String id,String text){
-		super();
-		this.id=id;
-		this.text=text;
-	}
-	/**
-	 * @param text 标题的文本
-	 */
-	public TemplateTitle(String text){
-		this(null, text);
-	}
+	private static final long serialVersionUID = 4531981977671625714L;
 
 
-	public String getType() {
-		return "template/title";
-	}
-
-	public Boolean getEscape() {
-		return this.escape;
-	}
-	
-	public TemplateTitle setEscape(Boolean escape) {
-		this.escape = escape;
-		return this;
-	}
-
-	public Boolean getScroll() {
-		return scroll;
-	}
-
-	public TemplateTitle setScroll(Boolean scroll) {
-		this.scroll = scroll;
-		return this;
-	}
-
-	public String getScrollClass() {
-		return scrollClass;
-	}
-	
-	public TemplateTitle setScrollClass(String scrollClass) {
-		this.scrollClass = scrollClass;
-		return this;
-	}
-	
-	/**
-	 * 显示文本
-	 * @return 本身，这样可以继续设置其他属性
-	 */
-	public String getText() {
-		return text;
-	}
-
-	/**
-	 * 显示文本
-	 * @param text 标题的文本
-	 * @return 本身，这样可以继续设置其他属性
-	 */
-	public TemplateTitle setText(String text) {
-		this.text = text;
-		return this;
-	}
-	
 }
