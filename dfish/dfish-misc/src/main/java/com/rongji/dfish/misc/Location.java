55e75507f6379fcccdc15082ebb48845ab738896
lamontYu <31650623@qq.com>
2019-05-09 16:51:03 +0800

    地图位置类MapPosition重命名成Location

--- dfish/dfish-misc/src/main/java/com/rongji/dfish/misc/Location.java
+++ dfish/dfish-misc/src/main/java/com/rongji/dfish/misc/Location.java
@@ -2,28 +2,47 @@ package com.rongji.dfish.misc;
 
 import java.io.Serializable;
 
-public class MapPosition implements Serializable {
 
-    public MapPosition() {
+/**
+ * 地理位置类,主要提供经纬度和位置等信息
+ *
+ * @author DFish Team
+ * @version 1.0
+ */
+public class Location implements Serializable {
+
+    public Location() {
 
     }
 
-    public MapPosition(double lng, double lat, String address) {
-        this.lng=lng;
-        this.lat=lat;
-        this.address=address;
+    public Location(double lng, double lat, String address) {
+        this.lng = lng;
+        this.lat = lat;
+        this.address = address;
     }
 
-    public MapPosition(double lng, double lat, int zoom, String address) {
-        this.lng=lng;
-        this.lat=lat;
-        this.zoom=zoom;
-        this.address=address;
+    public Location(double lng, double lat, int zoom, String address) {
+        this.lng = lng;
+        this.lat = lat;
+        this.zoom = zoom;
+        this.address = address;
     }
 
+    /**
+     * 经度longitude
+     */
     private double lng;
+    /**
+     * 纬度latitude
+     */
     private double lat;
+    /**
+     * 缩放比例
+     */
     private int zoom;
+    /**
+     * 具体地址信息
+     */
     private String address;
 
     public double getLng() {
@@ -57,4 +76,5 @@ public class MapPosition implements Serializable {
     public void setAddress(String address) {
         this.address = address;
     }
+
 }
