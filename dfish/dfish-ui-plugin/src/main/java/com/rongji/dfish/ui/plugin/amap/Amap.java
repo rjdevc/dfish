37ac63f8501941aa443f0d5b224c10b852d2f318
lamontYu <31650623@qq.com>
2019-05-10 09:58:57 +0800

    增加Amap类

---
+++ dfish/dfish-ui-plugin/src/main/java/com/rongji/dfish/ui/plugin/amap/Amap.java
@@ -0,0 +1,15 @@
+package com.rongji.dfish.ui.plugin.amap;
+
+import com.rongji.dfish.ui.AbstractWidget;
+
+/**
+ * 地图控件
+ */
+public class Amap extends AbstractWidget<Amap> {
+
+    @Override
+    public String getType() {
+        return "amap";
+    }
+
+}
