9ef10ebfe12dc6595fce947d90cd0bdef6046a17
LinLW <27542726@qq.com>
2019-08-07 18:05:41 +0800

    Option 增加checkall

--- dfish/dfish-ui/src/test/java/com/rongji/dfish/ui/form/XboxTest.java
+++ dfish/dfish-ui/src/test/java/com/rongji/dfish/ui/form/XboxTest.java
@@ -1,6 +1,8 @@
 package com.rongji.dfish.ui.form;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import com.rongji.dfish.ui.DFishUITestCase;
 
@@ -8,8 +10,11 @@ public class XboxTest extends DFishUITestCase{
 
 	@Override
 	protected Object getWidget() {
-		String[][] options={{"1","男"},{"2","女"},{"0","未确定或不可识别"},{"9","其他"}};
-		Xbox xbox=new Xbox("gender","性别","2",Arrays.asList(options));
+		List<String[]> options=Arrays.asList(new String[][]{{"1","男"},{"2","女"},{"0","未确定或不可识别"},{"9","其他"}});
+		List<Object>realOptions=new ArrayList<>();
+		realOptions.add(new Option("ALL","全部").setCheckall(true));
+		realOptions.addAll(options);
+		Xbox xbox=new Xbox("gender","性别","2",realOptions).setMultiple(true);
 		return xbox;
 	}
 
