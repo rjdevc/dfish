29dca8552e2aaa653c3d4b637cff6ab25fa3717d
lamontYu <31650623@qq.com>
2019-08-13 21:24:57 +0800

    小版本号升级,主要IE7兼容性问题的版本区分

--- dfish/dfish-framework/src/test/java/com/rongji/dfish/framework/config/CryptorTest.java
+++ dfish/dfish-framework/src/test/java/com/rongji/dfish/framework/config/CryptorTest.java
@@ -3,12 +3,13 @@ package com.rongji.dfish.framework.config;
 public class CryptorTest {
 
 	public static void main(String[] args) {
-		String[] strArray = new String[]{ "zhdjyf", "RJ123zhdjyf", "zhdj", "RJ123zhdj" };
+		String[] strArray = new String[]{ "rjtest", "test" };
 		
 		DefaultPropertyCryptor cryptor = new DefaultPropertyCryptor();
 		for (String str : strArray) {
 			System.out.println(str + ":[" + cryptor.encrypt(str) + "]");
 		}
+
 	}
 	
 }
--- dfish/dfish-framework/src/test/java/com/rongji/dfish/framework/plugin/file/service/FileServiceTest.java
+++ dfish/dfish-framework/src/test/java/com/rongji/dfish/framework/plugin/file/service/FileServiceTest.java
@@ -10,6 +10,11 @@ public class FileServiceTest {
 		String itemJson = "[{\"id\":\"v0oc2POsgVZsoo9CWABs9BMAHUsrBe46vgWy-8MpJ53-ykKUzf3e3w..\",\"name\":\"阿里巴巴Java开发手册(终极版).pdf\",\"size\":1056366}]";
 		List<UploadItem> items = FileService.parseUploadItems(itemJson);
 		System.out.println(items.size());
+
+		String enFileId = "fFC0LZPBKkAddlfMvrdSVomVNXT_pdOoY4mZhtcj1rL-ykKUzf3e3w..";
+
+		FileService fileService = new FileService();
+		System.out.println(fileService.decId(enFileId));
 		
 	}
 	
