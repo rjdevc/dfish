b708766419bd62966418a40f459b23677e8443d1
LinLW <27542726@qq.com>
2019-08-23 15:44:24 +0800

    由类反射改写，最大限度，减少代码重复。

--- dfish/dfish-misc/src/main/java/com/rongji/dfish/misc/docpreview/builder/SummaryBuilder.java
+++ dfish/dfish-misc/src/main/java/com/rongji/dfish/misc/docpreview/builder/SummaryBuilder.java
@@ -3,13 +3,17 @@ package com.rongji.dfish.misc.docpreview.builder;
 import com.rongji.dfish.base.util.BeanUtil;
 import com.rongji.dfish.misc.docpreview.data.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SummaryBuilder {
-    private int summaryScore =6000;
-    public static final int TYPE_DOCUMENT=0;
-    public static final int TYPE_CHARACTER=1;
+    public static final int TYPE_CHARACTER=0;
+    public static final int TYPE_DOCUMENT=1;
     public static final int TYPE_PARAGRAPH=2;
     public static final int TYPE_CHARACTER_RUN=3;
     public static final int TYPE_DRAWING=4;
@@ -17,14 +21,16 @@ public class SummaryBuilder {
     public static final int TYPE_ROW=6;
     public static final int TYPE_CELL=7;
     public static final int TYPE_COLUMN=8;
-    private int[] scores =new int[]{0,10,200,0,500,0,0,0,0};
+
+    private int summaryScore =6000;
+    private int[] scores =new int[]{10,0,200,0,500,0,0,0,0};
     public int getSummaryScore(){
         return summaryScore;
     }
     public int getScore(int type){
         return scores[type];
     }
-    public void setSummaryScore(){
+    public void setSummaryScore(int summaryScore){
         this.summaryScore=summaryScore;
     }
     public void setScore(int type,int score){
@@ -39,114 +45,63 @@ public class SummaryBuilder {
      * @return
      */
     public Document build(Document doc) {
-        Document d=new Document();
-        int left = summaryScore - scores[TYPE_DOCUMENT];
-        build(doc,d,left);
-        return d;
+        Document to=new Document();
+        AtomicInteger score=new AtomicInteger(0);
+        build(doc,score,to);
+        return to;
     }
-
-
-    private int build(Document from,Document to,int left){
-       List<DocumentElement> des=new ArrayList<>();
-       to.setBody(des);
-       for(DocumentElement de: from.getBody()){
-           if(left<=0)break;
-           if(de instanceof Paragraph){
-               left -= scores[TYPE_PARAGRAPH];
-               Paragraph toP=new Paragraph();
-               BeanUtil.copyPropertiesExact(toP,de);
-               des.add(toP);
-               left=build((Paragraph)de,toP,left);
-           }else if(de instanceof Table){
-               left -= scores[TYPE_TABLE];
-               Table toT=new Table();
-               BeanUtil.copyPropertiesExact(toT,de);
-               des.add(toT);
-               left=build((Table)de,toT,left);
-           }
-       }
-       return left;
+    private static final HashMap<Class,Object[]> REF_MAP=new HashMap<>();
+    static{
+        Object[][] types=new Object[][]{
+                {Document.class,TYPE_DOCUMENT,"Body"},
+                {Paragraph.class,TYPE_PARAGRAPH,"Body"},
+                {Table.class,TYPE_TABLE,"Rows"},
+                {TableRow.class,TYPE_ROW,"Cells"},
+                {TableCell.class,TYPE_CELL,"Body"},
+                {CharacterRun.class,TYPE_CHARACTER_RUN,null},
+                {Drawing.class,TYPE_DRAWING,null},
+        };
+        for(Object[] row :types){
+            REF_MAP.put((Class)row[0],row);
+        }
     }
-    private int build(Paragraph from,Paragraph to,int left){
-        List<ParagraphElement> pes=new ArrayList<>();
-        to.setBody(pes);
-        for(ParagraphElement pe: from.getBody()){
-            if(left<=0)break;
-            if(pe instanceof CharacterRun){
-                left -= scores[TYPE_CHARACTER_RUN];
-                CharacterRun toP=new CharacterRun();
-                BeanUtil.copyPropertiesExact(toP,pe);
-                pes.add(toP);
-                left=build((CharacterRun)pe,toP,left);
-            }else if(pe instanceof Drawing){
-                left -= scores[TYPE_DRAWING];
-                Drawing toT=new Drawing();
-                BeanUtil.copyPropertiesExact(toT,pe);
-                pes.add(toT);
-                left=build((Drawing)pe,toT,left);
+    private static final Class[] NO_PARAM=new Class[0];
+    private void build(Object from,AtomicInteger score,Object to){
+
+        BeanUtil.copyPropertiesExact(to,from);
+
+        Class<?>clz=from.getClass();
+        Object[] ref=REF_MAP.get(clz);
+        if(ref!=null){
+            int type=(Integer)ref[1];
+            String subName=(String)ref[2];
+
+            score.getAndAdd(scores[type]);
+            if(subName!=null){
+                try {
+                    Method getter=clz.getMethod("get"+subName,NO_PARAM);
+                    Method setter=clz.getMethod("set"+subName,List.class);
+                    List toSubs=new ArrayList();
+                    setter.invoke(to,toSubs);
+                    List fromSubs=(List)getter.invoke(from);
+                    for(Object fromSub: fromSubs) {
+                        if (score.get() >= summaryScore) {
+                            break;
+                        }
+                        Class subClz=fromSub.getClass();
+                        Object toSub= subClz.newInstance();
+                        build(fromSub,score,toSub);
+                        toSubs.add(toSub);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
-
-        return left;
-    }
-
-    private int build(Table from,Table to,int left){
-        List<TableRow> subs=new ArrayList<>();
-        to.setRows(subs);
-        for(TableRow pe: from.getRows()){
-            if(left<=0)break;
-            left -= scores[TYPE_ROW];
-            TableRow toP=new TableRow();
-            BeanUtil.copyPropertiesExact(toP,pe);
-            subs.add(toP);
-            left=build((TableRow)pe,toP,left);
+        if(clz==CharacterRun.class){
+            CharacterRun cr=(CharacterRun)from;
+            //越多的字符将额外计算积分。
+            score.getAndAdd(scores[TYPE_CHARACTER] * cr.getText().length());
         }
-        return left;
     }
-    private int build(TableRow from,TableRow to,int left){
-        List<TableCell> subs=new ArrayList<>();
-        to.setCells(subs);
-        for(TableCell pe: from.getCells()){
-            if(left<=0)break;
-            left -= scores[TYPE_CELL];
-            TableCell toP=new TableCell();
-            BeanUtil.copyPropertiesExact(toP,pe);
-            subs.add(toP);
-            left=build((TableCell)pe,toP,left);
-        }
-        return left;
-    }
-    private int build(TableCell from,TableCell to,int left){
-        List<DocumentElement> des=new ArrayList<>();
-        to.setBody(des);
-        for(DocumentElement de: from.getBody()){
-            if(left<=0)break;
-            if(de instanceof Paragraph){
-                left -= scores[TYPE_PARAGRAPH];
-                Paragraph toP=new Paragraph();
-                BeanUtil.copyPropertiesExact(toP,de);
-                des.add(toP);
-                left=build((Paragraph)de,toP,left);
-            }else if(de instanceof Table){
-                left -= scores[TYPE_TABLE];
-                Table toT=new Table();
-                BeanUtil.copyPropertiesExact(toT,de);
-                des.add(toT);
-                left=build((Table)de,toT,left);
-            }
-        }
-        return left;
-    }
-    private int build(CharacterRun from,CharacterRun to,int left){
-        //额外扣扣除每个字的分数
-        if(from.getText()!=null){
-            left -= from.getText().length() * scores[TYPE_CHARACTER];
-        }
-        return left;
-    }
-    private int build(Drawing from,Drawing to,int left){
-        return left;
-    }
-
-
 }
