cfa1c9fe6262af6c723cfcc93f4cf0208e14aa32
lamontYu <31650623@qq.com>
2019-09-03 11:30:35 +0800

    Ueditor增加option属性,支持自定义配置

--- dfish/dfish-ui-plugin/src/main/java/com/rongji/dfish/ui/plugin/ueditor/Ueditor.java
+++ dfish/dfish-ui-plugin/src/main/java/com/rongji/dfish/ui/plugin/ueditor/Ueditor.java
@@ -1,29 +1,59 @@
 package com.rongji.dfish.ui.plugin.ueditor;
 
 
-
 import com.rongji.dfish.ui.form.AbstractInput;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author LinLW
- *
  */
-public class Ueditor extends AbstractInput<Ueditor,String>{
+public class Ueditor extends AbstractInput<Ueditor, String> {
 
-	private static final long serialVersionUID = 3578548092631912403L;
-	@Override
-	public String getType() {
-		return "ueditor";
-	}
-	public Ueditor(String name, String label, String value) {
-	    this.setName(name);
-	    this.setValue(value);
-	    this.setLabel(label);
-	}
-	@Override
-	public Ueditor setValue(Object value) {
-		this.value=toString(value);
-		return this;
-	}
+    private static final long serialVersionUID = 3578548092631912403L;
+
+    private Map<String, Object> option;
+
+    @Override
+    public String getType() {
+        return "ueditor";
+    }
+
+    public Ueditor(String name, String label, String value) {
+        this.setName(name);
+        this.setValue(value);
+        this.setLabel(label);
+    }
+
+    @Override
+    public Ueditor setValue(Object value) {
+        this.value = toString(value);
+        return this;
+    }
+
+    public Map<String, Object> getOption() {
+        return option;
+    }
+
+    public Ueditor setOption(Map<String, Object> option) {
+        this.option = option;
+        return this;
+    }
+
+    public Ueditor putOption(String key, Object value) {
+        if (this.option == null) {
+            this.option = new HashMap<>();
+        }
+        this.option.put(key, value);
+        return this;
+    }
+
+    public Ueditor removeOption(String key) {
+        if (this.option != null) {
+            this.option.remove(key);
+        }
+        return this;
+    }
 
 }
