2e59d677c7d8530f071fd889b314880a91404838
lamontYu <31650623@qq.com>
2019-09-19 10:07:51 +0800

    CryptProvider默认秘钥调整

--- dfish/dfish-base/src/main/java/com/rongji/dfish/base/crypt/CryptProvider.java
+++ dfish/dfish-base/src/main/java/com/rongji/dfish/base/crypt/CryptProvider.java
@@ -1,13 +1,16 @@
 package com.rongji.dfish.base.crypt;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CryptProvider {
 
     private String algorithms = CryptFactory.ALGORITHMS_BLOWFISH;
     private String encoding = CryptFactory.ENCODING_UTF8;
     private int presentStyle = CryptFactory.PRESENT_STYLE_BASE32;
-    private String secretKey = "DFish";
+    private String secretKey = "DFish@RJ002474";
 
-    private StringCryptor cryptor;
+    private static final Map<String, StringCryptor> CRYPTORS = new HashMap<>();
 
     public String getAlgorithms() {
         return algorithms;
@@ -42,8 +45,11 @@ public class CryptProvider {
     }
 
     public StringCryptor getCryptor() {
+        String key = algorithms + "#" + encoding + "#" + presentStyle + "#" + secretKey;
+        StringCryptor cryptor = CRYPTORS.get(key);
         if (cryptor == null) {
             cryptor = CryptFactory.getStringCryptor(algorithms, encoding, presentStyle, secretKey);
+            CRYPTORS.put(key, cryptor);
         }
         return cryptor;
     }
--- dfish/dfish-framework/src/test/java/com/rongji/dfish/framework/config/CryptorTest.java
+++ dfish/dfish-framework/src/test/java/com/rongji/dfish/framework/config/CryptorTest.java
@@ -1,13 +1,25 @@
 package com.rongji.dfish.framework.config;
 
+import com.rongji.dfish.base.crypt.CryptFactory;
+import com.rongji.dfish.base.crypt.CryptProvider;
+
 public class CryptorTest {
 
 	public static void main(String[] args) {
-		String[] strArray = new String[]{ "rjtest", "test" };
-		
-		DefaultPropertyCryptor cryptor = new DefaultPropertyCryptor();
-		for (String str : strArray) {
-			System.out.println(str + ":[" + cryptor.encrypt(str) + "]");
+		String[] strArray = new String[]{ "test", "RJtest" };
+
+		CryptProvider cryptProvider = new CryptProvider();
+		String[] algorithmsArray = new String[]{
+				CryptFactory.ALGORITHMS_BLOWFISH,
+				CryptFactory.ALGORITHMS_SHA1,
+				CryptFactory.ALGORITHMS_SHA256,
+				CryptFactory.ALGORITHMS_SHA512
+		};
+		for (String algorithms : algorithmsArray) {
+			cryptProvider.setAlgorithms(algorithms);
+			for (String str : strArray) {
+				System.out.println("[" + algorithms + "]" + str + "->(" + cryptProvider.getCryptor().encrypt(str) + ")");
+			}
 		}
 
 	}
