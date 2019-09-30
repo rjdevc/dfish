585203cb2849bc66016c9299596f3b4a4949c878
LinLW <27542726@qq.com>
2019-09-30 16:42:44 +0800

    增补注释

--- dfish/dfish-ui/src/main/java/com/rongji/dfish/ui/layout/FormLayout.java
+++ dfish/dfish-ui/src/main/java/com/rongji/dfish/ui/layout/FormLayout.java
@@ -3,13 +3,27 @@ package com.rongji.dfish.ui.layout;
 import com.rongji.dfish.ui.*;
 import com.rongji.dfish.ui.form.Hidden;
 import com.rongji.dfish.ui.form.LabelRow;
-import com.rongji.dfish.ui.helper.FormPanel;
 import com.rongji.dfish.ui.layout.grid.Td;
 
 import java.util.Iterator;
 import java.util.List;
 
-public class FormLayout extends AbstractLayout<FormLayout,Widget<?>> implements HtmlContentHolder<FormLayout> ,MultiContainer<FormLayout,Widget<?>> {
+/**
+ * FormLayout 作为通用的带表格布局的表单。默认为12 列。插入的元素可以是1-12列。
+ * 可以通过设置getPub().setColspan(int)来设置默认的宽度。一般建议为4 12 或6.
+ * 现在如果 添加一个 超过12 (默认情况，下同) 的列，则自动被缩减为12列。
+ * 最后表单(FormLayout)上已有的最后一列，剩余的列 小于当前对象列大小。将会自动排到下一行。
+ * 如果有些表单元素，rowspan大于1，也会占用当前列的大小。排不下的时候，也会自动排列到下一行。
+ *
+ * 3.2之前，它 功能由 FormPanel FlexGrid LayoutFormPanel分摊。但3.3以后，如果使用前后端分离的模式开发，
+ * 则需要一个统一，方便的组件来代替，于是就封装了它。它现在对于前端是一个实际存在的类，不再是一个JsonWrapper。
+ * 3.3以后建议  FormPanel FlexGrid LayoutFormPanel 全部改为使用该类。
+ * @since 3.3
+ *
+ */
+public class FormLayout extends AbstractLayout<FormLayout,Widget<?>>
+        implements HtmlContentHolder<FormLayout>, Scrollable<FormLayout>,MultiContainer<FormLayout,Widget<?>> ,
+        PubHolder<FormLayout,Td> ,HiddenContainer<FormLayout>{
     /**
      * 无格式
      */
@@ -35,14 +49,24 @@ public class FormLayout extends AbstractLayout<FormLayout,Widget<?>> implements 
     private Boolean scroll;
     private String scrollClass;
 
+    /**
+     * 布局表格分成多少列。默认12
+     * @return Integer
+     */
     public Integer getCols() {
         return cols;
     }
 
+    /**
+     * 布局表格分成多少列。默认12
+     * @param cols Integer
+     * @return this
+     */
     public FormLayout setCols(Integer cols) {
         this.cols = cols;
         return this;
     }
+
     public Td getPub() {
         if (pub == null) {
             setPub(new Td());
@@ -83,6 +107,14 @@ public class FormLayout extends AbstractLayout<FormLayout,Widget<?>> implements 
         nodes.add(w);
         return this;
     }
+
+    /**
+     * 添加子元素，并且制定宽度。
+     * 相当于add(new Td().setColspan(colspan).setNode(w))
+     * @param w 元素
+     * @param colspan 占用宽度
+     * @return this
+     */
     public FormLayout add(Widget<?>w ,int colspan){
         if (w instanceof LabelRow) {
             if("0".equals(((LabelRow<?>) w).getLabel().getWidth())){
@@ -91,6 +123,15 @@ public class FormLayout extends AbstractLayout<FormLayout,Widget<?>> implements 
         }
         return add (new Td().setColspan(colspan).setNode(w));
     }
+
+    /**
+     * 添加子元素，并且制定宽度。
+     * 相当于add(new Td().setColspan(colspan).setRowspan(rowspan).setNode(w))
+     * @param w 元素
+     * @param colspan 占用宽度
+     * @param rowspan 占用高度
+     * @return
+     */
     public FormLayout add(Widget<?>w ,int colspan,int rowspan){
         if (w instanceof LabelRow) {
             if("0".equals(((LabelRow<?>) w).getLabel().getWidth())){
