92066feee195d11a8a16fad28bab97dfad858b58
LinLW <27542726@qq.com>
2019-10-16 15:15:46 +0800

    增加标签页

--- dfish-ui/src/main/java/com/rongji/dfish/ui/layout/Tabs.java
+++ dfish-ui/src/main/java/com/rongji/dfish/ui/layout/Tabs.java
@@ -1,6 +1,7 @@
 package com.rongji.dfish.ui.layout;
 
 import com.rongji.dfish.ui.*;
+import com.rongji.dfish.ui.form.Hidden;
 import com.rongji.dfish.ui.widget.Overflow;
 import com.rongji.dfish.ui.widget.Split;
 
@@ -10,7 +11,7 @@ import java.util.List;
  *  可切换标签容器
  */
 public class Tabs extends AbstractLayout<Tabs,Tab> implements MultiContainer<Tabs,Tab>,PubHolder<Tabs,Tab>,
-        Alignable<Tabs>,Valignable<Tabs> {
+        Alignable<Tabs>,Valignable<Tabs> ,HiddenContainer<Tabs>{
     private String align;
     private String valign;
     private  Integer space;
@@ -124,6 +125,42 @@ public class Tabs extends AbstractLayout<Tabs,Tab> implements MultiContainer<Tab
     @Override
     public Tabs setAlign(String align) {
         this.align = align;
+        return this;
+    }
+    /**
+     * 隐藏表单组
+     */
+    private HiddenPart hiddens = new HiddenPart();
+
+    @Override
+    public Tabs addHidden(String name, String value) {
+        hiddens.addHidden(name, value);
+        return this;
+    }
+    //	public T addHidden(String name,AtExpression value) {
+//		hiddens.addHidden(name, value);
+//		return (T)this;
+//	}
+//
+    @Override
+    public Tabs add(Hidden hidden) {
+        hiddens.add(hidden);
+        return this;
+    }
+
+    @Override
+    public List<Hidden> getHiddens() {
+        return hiddens.getHiddens();
+    }
+
+    @Override
+    public List<String> getHiddenValue(String name) {
+        return hiddens.getHiddenValue(name);
+    }
+
+    @Override
+    public Tabs removeHidden(String name) {
+        hiddens.removeHidden(name);
         return this;
     }
 }
