3ec21045c2b4bbfeb93e42f65183255388617b87
lamontYu <lamontYu@qq.com>
2019-11-25 11:48:05 +0800

    Toggle补充tip

--- dfish-ui/src/main/java/com/rongji/dfish/ui/widget/Toggle.java
+++ dfish-ui/src/main/java/com/rongji/dfish/ui/widget/Toggle.java
@@ -5,142 +5,193 @@ import com.rongji.dfish.ui.HasText;
 
 /**
  * 展开收拢的工具条
- * @author DFish Team
  *
+ * @author DFish Team
  */
-public class Toggle extends AbstractWidget<Toggle> implements HasText<Toggle>{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 4964103955404250558L;
+public class Toggle extends AbstractWidget<Toggle> implements HasText<Toggle> {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 4964103955404250558L;
 
-	@Override
-	public String getType() {
-		return "toggle";
-	}
-	
-	/**
-	 * 构造函数
-	 */
-	public Toggle() {
-		
-	}
-	
-	/**
-	 * 构造函数
-	 * @param text 文本
-	 */
-	public Toggle(String text) {
-		this.setText(text);
-	}
-	
-	private Boolean hr;
-	private Boolean open;
-	private String target;
-	private String text;
-	private String icon;
-	private String openicon;
+    @Override
+    public String getType() {
+        return "toggle";
+    }
 
-	/**
-	 * 是否显示一条水平线
-	 * @return hr
-	 */
-	public Boolean getHr() {
-		return hr;
-	}
-	
-	/**
-	 * 是否显示一条水平线
-	 * @param hr Boolean
-	 * @return 本身，这样可以继续设置其他属性
-	 */
-	public Toggle setHr(Boolean hr) {
-		this.hr = hr;
-		return this;
-	}
+    /**
+     * 构造函数
+     */
+    public Toggle() {
 
-	/**
-	 * 设置初始状态为展开或收拢。如果设置了此参数，会产生一个toggle图标
-	 * @return open
-	 */
-	public Boolean getOpen() {
-		return open;
-	}
+    }
 
-	/**
-	 * 设置初始状态为展开或收拢。如果设置了此参数，会产生一个toggle图标
-	 * @param open 设置初始状态是否展开
-	 * @return 本身，这样可以继续设置其他属性
-	 */
-	public Toggle setOpen(Boolean open) {
-		this.open = open;
-		return this;
-	}
-	/**
-	 * 绑定要展开收拢的 widget ID。多个用逗号隔开。
-	 * @return target
-	 */
-	public String getTarget() {
-		return target;
-	}
-	/**
-	 * 绑定要展开收拢的 widget ID。多个用逗号隔开。
-	 * @param target 绑定要展开收拢的 widget ID
-	 * @return 本身，这样可以继续设置其他属性
-	 */
-	public Toggle setTarget(String target) {
-		this.target = target;
-		return this;
-	}
-	/**
-	 * 显示文本
-	 * @return texts
-	 */
-	@Override
+    /**
+     * 构造函数
+     *
+     * @param text 文本
+     */
+    public Toggle(String text) {
+        this.setText(text);
+    }
+
+    private Boolean hr;
+    private Boolean open;
+    private String target;
+    private String text;
+    private String icon;
+    private String openicon;
+    private Object tip;
+
+    /**
+     * 是否显示一条水平线
+     *
+     * @return hr
+     */
+    public Boolean getHr() {
+        return hr;
+    }
+
+    /**
+     * 是否显示一条水平线
+     *
+     * @param hr Boolean
+     * @return 本身，这样可以继续设置其他属性
+     */
+    public Toggle setHr(Boolean hr) {
+        this.hr = hr;
+        return this;
+    }
+
+    /**
+     * 设置初始状态为展开或收拢。如果设置了此参数，会产生一个toggle图标
+     *
+     * @return open
+     */
+    public Boolean getOpen() {
+        return open;
+    }
+
+    /**
+     * 设置初始状态为展开或收拢。如果设置了此参数，会产生一个toggle图标
+     *
+     * @param open 设置初始状态是否展开
+     * @return 本身，这样可以继续设置其他属性
+     */
+    public Toggle setOpen(Boolean open) {
+        this.open = open;
+        return this;
+    }
+
+    /**
+     * 绑定要展开收拢的 widget ID。多个用逗号隔开。
+     *
+     * @return target
+     */
+    public String getTarget() {
+        return target;
+    }
+
+    /**
+     * 绑定要展开收拢的 widget ID。多个用逗号隔开。
+     *
+     * @param target 绑定要展开收拢的 widget ID
+     * @return 本身，这样可以继续设置其他属性
+     */
+    public Toggle setTarget(String target) {
+        this.target = target;
+        return this;
+    }
+
+    /**
+     * 显示文本
+     *
+     * @return texts
+     */
+    @Override
     public String getText() {
-		return text;
-	}
-	/**
-	 * 显示文本
-	 * @param text 显示文本
-	 * @return 本身，这样可以继续设置其他属性
-	 */
-	@Override
-    public Toggle setText(String text) {
-		this.text = text;
-		return this;
-	}
-	/**
-	 * 收缩图标
-	 * @return String
-	 */
-	public String getIcon() {
-		return icon;
-	}
-	/**
-	 * 收缩图标
-	 * @param icon  String icon
-	 * @return 本身，这样可以继续设置其他属性
-	 */
-	public Toggle setIcon(String icon) {
-		this.icon = icon;
-		return this;
-	}
-	/**
-	 * 展开图标
-	 * @return String
-	 */
-	public String getOpenicon() {
-		return openicon;
-	}
-	/**
-	 * 展开图标
-	 * @param openicon String
-	 * @return 本身，这样可以继续设置其他属性
-	 */
-	public Toggle setOpenicon(String openicon) {
-		this.openicon = openicon;
-		return this;
-	}
+        return text;
+    }
 
+    /**
+     * 显示文本
+     *
+     * @param text 显示文本
+     * @return 本身，这样可以继续设置其他属性
+     */
+    @Override
+    public Toggle setText(String text) {
+        this.text = text;
+        return this;
+    }
+
+    /**
+     * 收缩图标
+     *
+     * @return String
+     */
+    public String getIcon() {
+        return icon;
+    }
+
+    /**
+     * 收缩图标
+     *
+     * @param icon String icon
+     * @return 本身，这样可以继续设置其他属性
+     */
+    public Toggle setIcon(String icon) {
+        this.icon = icon;
+        return this;
+    }
+
+    /**
+     * 展开图标
+     *
+     * @return String
+     */
+    public String getOpenicon() {
+        return openicon;
+    }
+
+    /**
+     * 展开图标
+     *
+     * @param openicon String
+     * @return 本身，这样可以继续设置其他属性
+     */
+    public Toggle setOpenicon(String openicon) {
+        this.openicon = openicon;
+        return this;
+    }
+
+    /**
+     * 提示信息。设为true，提示信息将使用 text 参数的值。
+     * @return Object
+     */
+    public Object getTip() {
+        return tip;
+    }
+
+    /**
+     * 提示信息。设为true，提示信息将使用 text 参数的值。
+     *
+     * @param tip 提示信息
+     * @return 本身，这样可以继续设置其他属性
+     */
+    public Toggle setTip(String tip) {
+        this.tip = tip;
+        return this;
+    }
+
+    /**
+     * 提示信息。设为true，提示信息将使用 text 参数的值。
+     *
+     * @param tip 设为true，提示信息将使用 text 参数的值。
+     * @return 本身，这样可以继续设置其他属性
+     */
+    public Toggle setTip(Boolean tip) {
+        this.tip = tip;
+        return this;
+    }
 }
