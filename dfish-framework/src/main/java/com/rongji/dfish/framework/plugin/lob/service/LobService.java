f81d07e09b060d0ab543d6d7605e7d9206a38a98
lamontYu <lamontYu@qq.com>
2020-03-09 14:46:26 +0800

    字符转码异常不抛到外部

--- dfish-framework/src/main/java/com/rongji/dfish/framework/plugin/lob/service/LobService.java
+++ dfish-framework/src/main/java/com/rongji/dfish/framework/plugin/lob/service/LobService.java
@@ -9,15 +9,17 @@ import java.util.Map;
 
 /**
  * lob数据服务层接口
+ *
  * @author lamontYu
+ * @version 1.1 改造成接口模式,并调整方法名 lamontYu 2019-12-05
  * @date 2019-12-05
  * @since 3.2
- * @version 1.1 改造成接口模式,并调整方法名 lamontYu 2019-12-05
  */
 public interface LobService extends FrameworkService<PubLob, PubLob, String> {
 
     /**
      * 保存lob内容记录(字符串)
+     *
      * @param lobContent 内容
      * @return String 保存的编号
      */
@@ -26,12 +28,15 @@ public interface LobService extends FrameworkService<PubLob, PubLob, String> {
 
     /**
      * 保存lob内容（文件、图片）
+     *
      * @return String 保存的编号
      */
     String saveLobData(InputStream lobData) throws Exception;
+
     /**
      * 更新lob内容(字符串)
-     * @param lobId 编号
+     *
+     * @param lobId      编号
      * @param lobContent 内容
      * @return int 更新记录数
      */
@@ -39,7 +44,8 @@ public interface LobService extends FrameworkService<PubLob, PubLob, String> {
 
     /**
      * 更新lob内容(文件)
-     * @param lobId 编号
+     *
+     * @param lobId   编号
      * @param lobData 内容
      * @return int 更新记录数
      */
@@ -48,6 +54,7 @@ public interface LobService extends FrameworkService<PubLob, PubLob, String> {
 
     /**
      * 归档lob记录
+     *
      * @param lobId 编号
      * @return int 更新记录数
      */
@@ -56,6 +63,7 @@ public interface LobService extends FrameworkService<PubLob, PubLob, String> {
 
     /**
      * 获取lob内容(文件)
+     *
      * @param lobId 编号
      * @return String lob内容值
      */
@@ -63,6 +71,7 @@ public interface LobService extends FrameworkService<PubLob, PubLob, String> {
 
     /**
      * 批量获取lob内容(文件)
+     *
      * @param lobIds 编号数组
      * @return Map&lt;String, String&gt; 编号-内容的键值对集合
      */
@@ -70,6 +79,7 @@ public interface LobService extends FrameworkService<PubLob, PubLob, String> {
 
     /**
      * 批量获取lob内容(文件)
+     *
      * @param lobIds 编号集合
      * @return Map&lt;String, String&gt; 编号-内容的键值对集合
      */
@@ -77,23 +87,26 @@ public interface LobService extends FrameworkService<PubLob, PubLob, String> {
 
     /**
      * 获取lob内容
+     *
      * @param lobId 编号
      * @return String lob内容值
      */
-    String getContent(String lobId) throws  Exception;
+    String getContent(String lobId);
 
     /**
      * 批量获取lob内容
+     *
      * @param lobIds 编号数组
      * @return Map&lt;String, String&gt; 编号-内容的键值对集合
      */
-    Map<String, String> getContents(String... lobIds) throws  Exception;
+    Map<String, String> getContents(String... lobIds);
 
     /**
      * 批量获取lob内容
+     *
      * @param lobIds 编号集合
      * @return Map&lt;String, String&gt; 编号-内容的键值对集合
      */
-    Map<String, String> getContents(Collection<String> lobIds) throws  Exception;
+    Map<String, String> getContents(Collection<String> lobIds);
 
 }
