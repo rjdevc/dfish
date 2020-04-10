fc57993bd87de4b58bdcca6a7f5a9924fad983f3
chenmingyuan <bless0215@163.com>
2020-04-10 11:29:03 +0800

    API更新

--- dfish-engine/src/main/webapp/api/js/api-data.js
+++ dfish-engine/src/main/webapp/api/js/api-data.js
@@ -1637,7 +1637,7 @@ define( {
     Config: [
       { name: 'badge', type: 'Boolean | String | Badge', remark: '显示徽标。' },
       { name: 'closable', type: 'Boolean', remark: '是否有关闭图标。' },
-      { name: 'dir', type: 'String', remark: '图片展现方式。可选值: <b>h</b>, <b>v</b>。默认值为h' },
+      { name: 'dir', type: 'String', remark: '排列方式。可选值: <b>h</b><s>(横排)</s>, <b>v</b><s>(竖排)</s>。默认值为h' },
       { name: 'escape', type: 'Boolean', remark: '是否对html内容转义。默认值为true。' },
       { name: 'format', type: 'String', remark: '格式化文本内容。"$字段名"形式的变量将被解析替换。支持"javascript:"开头的js语句(需return返回值)。' },
       { name: 'focus', type: 'Boolean', remark: '是否焦点模式。' },
@@ -3205,7 +3205,7 @@ define( {
       ] },
       { name: 'description', type: 'String', remark: '图片说明。当 album face="straight" 时会显示说明。' },
       { name: 'escape', type: 'Boolean', remark: '是否对html内容转义。默认值为true。' },
-      { name: 'dir', type: 'String', remark: '图片展现方式。可选值: <b>h</b>, <b>v</b>。默认值为v' },
+      { name: 'dir', type: 'String', remark: '排列方式。可选值: <b>h</b><s>(横排)</s>, <b>v</b><s>(竖排)</s>。默认值为h' },
       { name: 'focusable', type: 'Boolean', remark: '是否可选中。' },
       { name: 'format', type: 'String', remark: '格式化文本内容。"$字段名"形式的变量将被解析替换。支持"javascript:"开头的js语句(需return返回值)。' },
       { name: 'imgWidth', type: 'Number | String', remark: '图片宽度。' },
