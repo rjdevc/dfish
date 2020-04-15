5fdc5fe86c968807523ffec4d04356b3d0ca4943
lamontYu <lamontYu@qq.com>
2020-04-15 21:17:54 +0800

    对话框模板文件补充

---
+++ dfish-engine/src/main/webapp/p/alert.js
@@ -0,0 +1,29 @@
+$.preload("g/alert",{
+	"type":"dialog","cls":"dlg","node":{
+		"type":"view","commands":{
+			"close":{"type":"js","text":"dfish.close(this);"}
+		},"node":{
+			"type":"vert","id":"dlg_frame","height":"*","width":"*","nodes":[
+				{
+					"type":"horz","id":"dlg_head","cls":"dlg-head","height":"40","valign":"middle","nodes":[
+						{"type":"dialog/title","id":"dlg_title","cls":"dlg-title","width":"*","wmin":10},{
+							"type":"buttonbar","id":"dlg_head_oper","cls":"dlg-head-oper","width":"-1","align":"center","valign":"middle","pub":{"height":"*","width":"40"},"nodes":[
+								{
+									"tip":"关闭","cls":"x-dlg-close","on":{"click":"this.cmd('close');"},"icon":".i-dlg-close"
+								}
+							]
+						}
+					]
+				},{
+					"type":"vert","id":"dlg_trunk","height":"*","nodes":[
+						{
+							"type":"vert","height":"*","nodes":[
+								{"type":"preload/body","id":"dlg_body","height":"*"}
+							]
+						}
+					]
+				}
+			]
+		}
+	}
+});
