887a5a58b0dbd10b82395c781ac26aa2db0d1084
chenmingyuan <43588313@qq.com>
2020-05-12 16:00:55 +0800

    echarts支持javascript:return

--- dfish-engine/src/main/webapp/pl/echarts/echarts.dfish.js
+++ dfish-engine/src/main/webapp/pl/echarts/echarts.dfish.js
@@ -2,22 +2,33 @@
  * {type: 'echarts', option: {}}
  */
 
-var echarts = require( './echarts.min' );
+var echarts = require( './echarts.min' ),
+	Q = require( 'jquery' );
 
-define.widget( 'echarts', {
-    Listener: {
-        body: {
-            ready: function() {
-                this.x.option && this.init( this.x.option );
-            },
-            resize: function() {
-                this.echarts && this.echarts.resize();
-            }
-        }
-    },
-    Prototype: {
-        init: function( opt ) {
-            (this.echarts = echarts.init( this.$() )).setOption( opt );
-        }
-    }
+define.widget( 'ECharts', {
+	Listener: {
+		body: {
+			ready: function() {
+				this.x.option && this.init( this.x.option );
+			},
+			resize: function() {
+				this.echarts && this.echarts.resize();
+			}
+		}
+	},
+	Prototype: {
+		init: function( opt ) {
+			(this.echarts = echarts.init( this.$() )).setOption( this.parseOption( opt ) );
+		},
+		// ½âÎö "javscript:return"
+		parseOption: function( o ) {
+			for ( var k in o ) {
+				if ( typeof o[ k ] === 'string' ) {
+					if ( o[ k ].indexOf( 'javascript:' ) === 0 ) o[ k ] = this.formatJS( o[ k ] );
+				} else if ( Q.isPlainObject( o[ k ] ) )
+					this.parseOption( o[ k ] );
+			}
+			return o;
+		}
+	}
 } );
