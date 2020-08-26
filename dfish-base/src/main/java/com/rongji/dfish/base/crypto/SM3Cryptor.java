580071353b780a17efce41deff5986c8d5a98f67
匿名 <27542726@qq.com>
2020-08-26 17:32:32 +0800

    增加注释，改进命名

--- dfish-base/src/main/java/com/rongji/dfish/base/crypto/SM3Cryptor.java
+++ dfish-base/src/main/java/com/rongji/dfish/base/crypto/SM3Cryptor.java
@@ -6,15 +6,20 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.util.Arrays;
 
+/**
+ * 国密3 数字摘要算法。
+ * https://gitee.com/snowlandltd/utilx4j/blob/master/src/main/java/ltd/snowland/security/SM3.java
+ * 算法来源为以上开源地址。
+ * 但以上地址，对JAVA的流模式处理比较弱，故在形式上做了较大改动。
+ * 核心算法和 初始IV 轮转参数 以及 padding补码规则皆与之相同。
+ */
 public class SM3Cryptor extends  AbstractCryptor {
     @Override
     protected void doEncrypt(InputStream is, OutputStream os) {
         try {
-
             this.reset();
             byte[] b = new byte[1024 * 8];
             int len = 0;
@@ -67,9 +72,6 @@ public class SM3Cryptor extends  AbstractCryptor {
         super(db);
     }
 
-
-
-
     private static final byte[] IV = new BigInteger(
             "7380166f" +
                     "4914b2b9" +
@@ -81,84 +83,51 @@ public class SM3Cryptor extends  AbstractCryptor {
                     "b0fb0e4e", 16).toByteArray();
     private static final int TJ_15 = 0x79cc4519;
     private static final int TJ_63 = 0x7a879d8a;
-
     private static final byte FIRST_PADDING =  (byte) 0x80 ;
     private static final byte ZERO_PADDING =  (byte) 0x00 ;
 
-    private static int T(int j) {
-        if(j>=64){
-            throw new RuntimeException("data invalid");
-        }
-        if(j>=16){
-            return TJ_63;
-        }
-        if(j>=0){
-            return TJ_15;
-        }
-        throw new RuntimeException("data invalid");
-    }
 
-    private static int FF(int x, int y, int z, int j) {
-        if(j>=64){
-            throw new RuntimeException("data invalid");
-        }
-        if(j>=16){
-            return (x & y) | (x & z) | (y & z);
-        }
-        if(j>=0){
-            return x ^ y ^ z;
-        }
-        throw new RuntimeException("data invalid");
-    }
 
-    private static int GG(int x, int y, int z, int j) {
-        if(j>=64){
-            throw new RuntimeException("data invalid");
-        }
-        if(j>=16){
-            return (x & y) | (~x & z) ;
-        }
-        if(j>=0){
-            return x ^ y ^ z;
-        }
-        throw new RuntimeException("data invalid");
-    }
-
-    private static int P0(int x) {
-        return x ^ Integer.rotateLeft(x, 9) ^ Integer.rotateLeft(x, 17);
-    }
-
-    private static int P1(int x) {
-        return x ^ Integer.rotateLeft(x, 15) ^ Integer.rotateLeft(x, 23);
-    }
-
-    byte[] value=null;
+    protected byte[] value=null;
     protected byte[] buff;
     protected int buffLen;
     protected long totalByte;
-    private void reset(){
+
+    /**
+     * 重置、初始化
+     * @see java.security.MessageDigest#reset()
+     */
+    protected void reset(){
         value=IV;
         buffLen=0;
         buff=new byte[64];
     }
-    private void update(byte[] source,int offset,int len){
+
+    /**
+     * 流模式的每段更新
+     * @param source 内容
+     * @param offset 偏移量
+     * @param len 长度
+     * @see java.security.MessageDigest#update(byte[], int, int)
+     */
+    protected void update(byte[] source,int offset,int len){
         totalByte+=len;
-        int consum=0;
+        int consume=0;
         if (buffLen > 0) {
-            consum = 64 - buffLen;
-            if(len<consum){
+            consume = 64 - buffLen;
+            if(len<consume){
                 System.arraycopy(source, 0, buff, buffLen, len);
                 buffLen +=len;
                 return;
             }
-            System.arraycopy(source, 0, buff, buffLen, consum);
-            value = CF(value, source, offset);
+            System.arraycopy(source, 0, buff, buffLen, consume);
+            value = cf(value, source, offset);
             buffLen =0;//使用了。
         }
-        offset += consum;
+        offset += consume;
 
         while(offset<=len-64) {
-            value = CF(value, source, offset);
+            value = cf(value, source, offset);
             offset+=64;
         }
         if (offset > len - 64) {
@@ -168,8 +137,13 @@ public class SM3Cryptor extends  AbstractCryptor {
             }
         }
     }
-    private byte[] digest(){
-//        totalByte*8;
+
+    /**
+     * 获取摘要结果
+     * @return byte[]
+     * @see java.security.MessageDigest#digest()
+     */
+    protected byte[] digest(){
         //增加一个FIRST_PADDING  和若干个字节的0 。最后8个字节为整个的bit长度
         //加入buffLen大于55 则 可能导致最后CF两次。
         buff[buffLen]=FIRST_PADDING;
@@ -183,76 +157,41 @@ public class SM3Cryptor extends  AbstractCryptor {
             for(;buffLen<64;buffLen++){
                 buff[buffLen]=ZERO_PADDING;
             }
-            value = CF(value, buff, 0);
+            value = cf(value, buff, 0);
             Arrays.fill(buff,0,56,ZERO_PADDING);
         }
         byte[]lenthPadding=toByteArray(totalByte*8);
         System.arraycopy(lenthPadding,0,buff,56,8);
-        value = CF(value, buff, 0);
+        value = cf(value, buff, 0);
 
         return value;
     }
 
-
-
-//    @Deprecated
-//    private static byte[] padding(byte[] source) throws IOException {
-//        if (source.length >= 0x2000000000000000l) {
-//            throw new RuntimeException("src data invalid.");
-//        }
-//        long l = source.length * 8;
-//        long k = 448 - (l + 1) % 512;
-//        if (k < 0) {
-//            k = k + 512;
-//        }
-//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//        baos.write(source);
-//        baos.write(FIRST_PADDING);
-//        long i = k - 7;
-//        while (i > 0) {
-//            baos.write(ZERO_PADDING);
-//            i -= 8;
-//        }
-//        baos.write(toByteArray(l));
-//        return baos.toByteArray();
-//    }
-//
-//
-//
-//    //主入口
-//    public static byte[] hash(byte[] source) throws IOException {
-//        byte[] m1 = padding(source);
-//        int n = m1.length / 64;
-//        byte[] b;
-//        byte[] vi = IV;
-//        byte[] vi1 = null;
-//        for (int i = 0; i < n; i++) {
-//            b = Arrays.copyOfRange(m1, i * 64, (i + 1) * 64);
-//            vi1 = CF(vi, b);
-//            vi = vi1;
-//        }
-//        return vi1;
-//    }
-
-
-    private static byte[] CF(byte[] vi, byte[] bi,int offset) {
+    /**
+     * 单轮摘要(核心算法)
+     * @param vi 已有结果
+     * @param bi 原文内容
+     * @param offset 本轮64byte的初始位置
+     * @return
+     */
+    protected static byte[] cf(byte[] vi, byte[] bi, int offset) {
         int a, b, c, d, e, f, g, h;
         a = toInteger(vi, 0);
-        b = toInteger(vi, 1);
-        c = toInteger(vi, 2);
-        d = toInteger(vi, 3);
-        e = toInteger(vi, 4);
-        f = toInteger(vi, 5);
-        g = toInteger(vi, 6);
-        h = toInteger(vi, 7);
+        b = toInteger(vi, 4);
+        c = toInteger(vi, 8);
+        d = toInteger(vi, 12);
+        e = toInteger(vi, 16);
+        f = toInteger(vi, 20);
+        g = toInteger(vi, 24);
+        h = toInteger(vi, 28);
 
         int[] w = new int[68];
         int[] w1 = new int[64];
         for (int i = 0; i < 16; i++) {
-            w[i] = toInteger(bi, i, offset);
+            w[i] = toInteger(bi, i*4+ offset);
         }
         for (int j = 16; j < 68; j++) {
-            w[j] = P1(w[j - 16] ^ w[j - 9] ^ Integer.rotateLeft(w[j - 3], 15)) ^ Integer.rotateLeft(w[j - 13], 7)
+            w[j] = p1(w[j - 16] ^ w[j - 9] ^ Integer.rotateLeft(w[j - 3], 15)) ^ Integer.rotateLeft(w[j - 13], 7)
                     ^ w[j - 6];
         }
         for (int j = 0; j < 64; j++) {
@@ -260,10 +199,10 @@ public class SM3Cryptor extends  AbstractCryptor {
         }
         int ss1, ss2, tt1, tt2;
         for (int j = 0; j < 64; j++) {
-            ss1 = Integer.rotateLeft(Integer.rotateLeft(a, 12) + e + Integer.rotateLeft(T(j), j), 7);
+            ss1 = Integer.rotateLeft(Integer.rotateLeft(a, 12) + e + Integer.rotateLeft(t(j), j), 7);
             ss2 = ss1 ^ Integer.rotateLeft(a, 12);
-            tt1 = FF(a, b, c, j) + d + ss2 + w1[j];
-            tt2 = GG(e, f, g, j) + h + ss1 + w[j];
+            tt1 = ff(a, b, c, j) + d + ss2 + w1[j];
+            tt2 = gg(e, f, g, j) + h + ss1 + w[j];
             d = c;
             c = Integer.rotateLeft(b, 9);
             b = a;
@@ -271,7 +210,7 @@ public class SM3Cryptor extends  AbstractCryptor {
             h = g;
             g = Integer.rotateLeft(f, 19);
             f = e;
-            e = P0(tt2);
+            e = p0(tt2);
         }
         byte[] v = toByteArray(a, b, c, d, e, f, g, h);
         for (int i = 0; i < v.length; i++) {
@@ -279,18 +218,65 @@ public class SM3Cryptor extends  AbstractCryptor {
         }
         return v;
     }
-    private static int toInteger(byte[] source, int index,int offset) {
-        return (source[index*4+0+offset]&0xff)<<24|
-                (source[index*4+1+offset]&0xff)<<16|
-                (source[index*4+2+offset]&0xff)<<8|
-                (source[index*4+3+offset]&0xff);
+
+
+    /**
+     * 取得轮转参数
+     * @param pos 位置。
+     * @return int
+     */
+    private static int t(int pos) {
+        if(pos>=64){
+            throw new RuntimeException("data invalid");
+        }
+        if(pos>=16){
+            return TJ_63;
+        }
+        if(pos>=0){
+            return TJ_15;
+        }
+        throw new RuntimeException("data invalid");
     }
 
-    private static int toInteger(byte[] source, int index) {
-        return (source[index*4+0]&0xff)<<24|
-                (source[index*4+1]&0xff)<<16|
-                (source[index*4+2]&0xff)<<8|
-                (source[index*4+3]&0xff);
+    private static int ff(int x, int y, int z, int pos) {
+        if(pos>=64){
+            throw new RuntimeException("data invalid");
+        }
+        if(pos>=16){
+            return (x & y) | (x & z) | (y & z);
+        }
+        if(pos>=0){
+            return x ^ y ^ z;
+        }
+        throw new RuntimeException("data invalid");
+    }
+
+    private static int gg(int x, int y, int z, int pos) {
+        if(pos>=64){
+            throw new RuntimeException("data invalid");
+        }
+        if(pos>=16){
+            return (x & y) | (~x & z) ;
+        }
+        if(pos>=0){
+            return x ^ y ^ z;
+        }
+        throw new RuntimeException("data invalid");
+    }
+
+    private static int p0(int x) {
+        return x ^ Integer.rotateLeft(x, 9) ^ Integer.rotateLeft(x, 17);
+    }
+
+    private static int p1(int x) {
+        return x ^ Integer.rotateLeft(x, 15) ^ Integer.rotateLeft(x, 23);
+    }
+
+    private static int toInteger(byte[] source, int offset) {
+        return (source[offset]&0xff)<<24|
+                (source[offset+1]&0xff)<<16|
+                (source[offset+2]&0xff)<<8|
+                (source[offset+3]&0xff);
     }
 
     private static byte[] toByteArray(int a, int b, int c, int d, int e, int f, int g, int h)  {
@@ -311,12 +297,12 @@ public class SM3Cryptor extends  AbstractCryptor {
         }
     }
 
-    public static byte[] toByteArray(int i) {
+    private static byte[] toByteArray(int i) {
         byte[] byteArray = new byte[4];
         byteArray[0] = (byte) (i >>> 24);
-        byteArray[1] = (byte) ((i & 0xFFFFFF) >>> 16);
-        byteArray[2] = (byte) ((i & 0xFFFF) >>> 8);
-        byteArray[3] = (byte) (i & 0xFF);
+        byteArray[1] = (byte) (i >>> 16);
+        byteArray[2] = (byte) (i >>> 8);
+        byteArray[3] = (byte) i;
         return byteArray;
     }
     private static byte[] toByteArray(long l) {
